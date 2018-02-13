f9df2bc97b945a88769e21a7909d32516fef50de
LukaszWiszniewski <wisznu07@gmail.com>
2018-02-13 12:17:10 +0100

    Fixing errors in contacts.js and app.js

--- components/Contact.js
+++ components/Contact.js
@@ -7,9 +7,9 @@ var Contact = React.createClass({
             <div className = "contactItem">
                 <img className = "contactImage" src= 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'/>
                 <div className = "contactInfo">
-                    <p className = "contactLabel"></p>
-                    <p className = "contactLabel"></p>
-                    <a className = "contactEmail"></a>
+                    <p className = "contactLabel" placeholder='Imie'>{this.props.item.firstName}</p>
+                    <p className = "contactLabel" placeholder='Nazwisko'>{this.props.item.lastName}</p>
+                    <a className = "contactEmail" href={ 'mailto:'+this.props.item.email}>{this.props.item.email}</a>
                 </div>
             </div>
 
